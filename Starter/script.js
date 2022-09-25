3371592785dff0610b7db163b8cb575789cc4876
Ismail <mdismailhossain215@gmail.com>
2022-09-25 10:04:34 +0600

    Starting the coding

---
+++ Starter/script.js
@@ -0,0 +1,24 @@
+//HW
+console.log("Hello World!");
+
+//Values and Variable
+let js = "amazing";
+console.log(js);
+console.log(2 + 5 + 7 + 4 - 6);
+
+let firstName = "Ismail";
+let lastName = "Hossain";
+console.log(firstName + " " + lastName);
+
+//Data Types
+console.log(true);
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Ismail');
+
+
+javaScriptIsFun = "YES!"
+console.log(typeof javaScriptIsFun);

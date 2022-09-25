06b56c5651274e35b3aac28f81c1c6694f788917
Ismail <mdismailhossain215@gmail.com>
2022-09-25 10:24:54 +0600

    Basic Operators, Assignment Operators, Math Operators

--- Starter/script.js
+++ Starter/script.js
@@ -31,3 +31,33 @@ console.log(typeof year);
 
 console.log(typeof null);
 
+//let var const
+//Basic Operators
+const ageIsmail = 2022 - 2000;
+const ageJonas = 2022 - 1991;
+console.log(ageIsmail, ageJonas);
+
+const now = 2022;
+const birthYear = 2000;
+console.log(now - birthYear);
+
+console.log(2 ** 3);
+//2**3 means 2 to the power 3
+
+//Assignment operators
+let x = 5;
+x += 5; //x= x+5
+console.log(x);
+x -= 5; //x= x-5
+console.log(x);
+x *= 5; //x = x*5
+console.log(x);
+x /= 5; // x= x/5
+console.log(x);
+
+x++;
+console.log(x);
+x--;
+console.log(x);
+
+//Comparison Operators
